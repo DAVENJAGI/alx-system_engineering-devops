d9c83d1850ed88d62cabe7961ee086221ecda26c
DAVENJAGI <njagidave39@gmail.com>
2023-11-24 16:34:53 -0500

    Puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create a file.
 
 file { '/tmp/school':
-ensure  => 'present',
-content => 'I love Pupper',
-mode    => '0744',
-owner   => 'www-data',
-group   => 'www-data',
+  ensure  => 'present',
+  content => 'I love Pupper',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,10 @@
 #Using Puppet, install flask from pip3
 
 package { 'python3-pip':
-ensure   => 'installed',
+  ensure   => 'installed',
 }
 
 package { 'flask':
-ensure   => '2.1.0'
-provider => 'pip',
+  ensure   => '2.1.0'
+  provider => 'pip',
 }
