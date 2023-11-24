dd735c988d737135f46f56fb8c8a263927459853
DAVENJAGI <njagidave39@gmail.com>
2023-11-24 17:01:17 -0500

    Puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 #Using Puppet, install flask from pip3
-#must be version 2.1.0
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+package { 'Flask':
+  ensure  => '2.1.0',
+  provider=> 'pip3',
 }
