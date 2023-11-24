09f1cb9b78397eb31799f317c9333f1177ed4667
DAVENJAGI <njagidave39@gmail.com>
2023-11-24 16:43:09 -0500

    Puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,10 @@
-# create a file.
+# create a file in /tmp
+#file sould have permission 0744
+
 file { '/tmp/school':
   ensure  => present,
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',}
+  content => 'I love Puppet',
+}
