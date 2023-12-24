36a97728bfed198e3bc32973704acb61cb27c33c
DAVENJAGI <njagidave39@gmail.com>
2023-12-24 08:54:29 -0500

    MySQL

---
+++ 0x14-mysql/userhbt.sql
@@ -0,0 +1,5 @@
+-- this is a sql script that creates a user holberton_user on web servers at localhost that allows access to server
+-- user has permission to check primary/replica status of the database
+CREATE USER IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
+GRANT REPLICATE CLIENT ON *.* TO 'holberton_user'@'localhost';
+FLUSH PRIVILEGES
