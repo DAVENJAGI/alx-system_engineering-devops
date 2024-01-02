f2ba6e4d5f112bdbcc5279a5b2580117a5984215
DAVENJAGI <njagidave39@gmail.com>
2024-01-02 19:15:29 +0300

    API

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,25 +9,18 @@ employee_ID = sys.argv[1]
 
 # a get request to get the users data
 url = f"https://jsonplaceholder.typicode.com/users/{employee_ID}"
-user_response = requests.get(url).json()
+user_response = requests.get(url)
 
 url_1 = f'https://jsonplaceholder.typicode.com/users/{employee_ID}/todos'
-
 user_response_1 = requests.get(url_1)
-to_do_data = user_response_1.json()
+to_data = user_response_1.json()
 
-with open("2.csv", "w", Newline="") as file:
-    fieldname = ["USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"]
-    writer = csv.DictWriter(file, fieldname=fieldname)
-    writer.writeheader()
-    
-    for task in to_do_data:
-        writer.writerow({
-            "USER_ID": employee_id,
-            "USERNAME": employee_name,
-            "TASK_COMPLETED_STATUS": str(task["completed"])
-            "TASK_TITLE": task["title"]
-            })
+f_name = "{}.csv".format(employee_ID)
+with open(f_name, "w", Newline="") as file:
+    writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+    for task in to_data:
+        writer.writerow([user["id"], user["username"],
+                         to_data["completed"], to_data["title"]])
 
 
 if __name__ == "__main__":
