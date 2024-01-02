ccf35bcb35b7ced2c2b8f06683349ef5fa1bbb89
DAVENJAGI <njagidave39@gmail.com>
2024-01-02 20:32:13 +0300

    API_Task_1n

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
+"""
+converts dta to csv format
+"""
 
 import csv
 import json
 import requests
 import sys
 
-employee_ID = sys.argv[1]
 
-# a get request to get the users data
-url = f"https://jsonplaceholder.typicode.com/users/{employee_ID}"
-user_response = requests.get(url)
+def change_to_csv(employee_ID):
+    """Function that converts data to csv format"""
+    # a get request to get the users data
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_ID}"
+    user_response = requests.get(url)
 
-url_1 = f'https://jsonplaceholder.typicode.com/users/{employee_ID}/todos'
-user_response_1 = requests.get(url_1)
-to_data = user_response_1.json()
+    url_1 = f'https://jsonplaceholder.typicode.com/users/{employee_ID}/todos'
+    user_response_1 = requests.get(url_1)
+    to_data = user_response_1.json()
 
-f_name = "{}.csv".format(employee_ID)
-with open(f_name, "w", Newline="") as file:
-    writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-    for task in to_data:
-        writer.writerow([user["id"], user["username"],
-                         to_data["completed"], to_data["title"]])
+    f_name = "{}.csv".format(employee_ID)
+    with open(f_name, "w") as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in to_data:
+            writer.writerow([user["id"], user["username"],
+                             to_data["completed"], to_data["title"]])
 
 
 if __name__ == "__main__":
-    pass
+    change_to_csv(int(sys.argv[1]))
