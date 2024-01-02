a2e279170276240bd402c3e9116e04eddbdb357c
DAVENJAGI <njagidave39@gmail.com>
2024-01-02 20:48:41 +0300

    API_Task_1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -14,6 +14,7 @@ def change_to_csv(employee_ID):
     # a get request to get the users data
     url = f"https://jsonplaceholder.typicode.com/users?id="
     user_response = requests.get(url)
+    user = user_response.json()
 
     url_1 = f'https://jsonplaceholder.typicode.com/users/{employee_ID}/todos'
     user_response_1 = requests.get(url_1)
@@ -22,10 +23,9 @@ def change_to_csv(employee_ID):
     f_name = "{}.csv".format(employee_ID)
     with open(f_name, "w") as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        for to_do in to_dos:
+        for to_do in user:
             writer.writerow([user["id"], user["username"],
                              to_do["completed"], to_do["title"]])
-
 
 if __name__ == "__main__":
     change_to_csv(int(sys.argv[1]))
