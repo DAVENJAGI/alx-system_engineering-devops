cd1a97baa5c6efac92ae1e34cda7b373c29c8a1c
DAVENJAGI <njagidave39@gmail.com>
2024-01-02 22:18:41 +0300

    API_Task_2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+converts data to json format
+"""
+
+import csv
+import json
+import requests
+import sys
+
+
+def dump_to_json(employee_ID):
+    """Function that converts data to csv format"""
+    # a get request to get the users data
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_ID}"
+    user_response = requests.get(url)
+    user = user_response.json()
+
+    url_1 = f'https://jsonplaceholder.typicode.com/users/{employee_ID}/todos'
+    user_response_1 = requests.get(url_1)
+    to_dos = user_response_1.json()
+
+    f_name = "{}.json".format(employee_ID)
+    with open(f_name, "w") as file:
+        data = {
+                employee_ID: [
+                    {"task": task["title"], "completed":
+                     str(task["completed"]), "username": user["name"]}
+                    for task in to_dos]
+                }
+        json.dump(data, file, indent=4)
+
+
+if __name__ == "__main__":
+    dump_to_json(int(sys.argv[1]))
