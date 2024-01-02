d11ad5a8b8a18d0414fce97b8d2d804cb28144b7
DAVENJAGI <njagidave39@gmail.com>
2024-01-02 19:32:50 +0300

    API_Task_0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,31 +8,37 @@ import sys
 
 employee_ID = sys.argv[1]
 
-# a get request to get the users data
-url = f"https://jsonplaceholder.typicode.com/users/{employee_ID}"
-user_response = requests.get(url)
 
-# parsing the user data to json format
-user_data = user_response.json()
+def todo_list(employee_ID):
+    """A function in REST API for to do list progress"""
 
-""" a get request to get the user's
-to do data from /users/employee_ID and parsing it to json"""
-url_1 = f'https://jsonplaceholder.typicode.com/users/{employee_ID}/todos'
+    # a get request to get the users data
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_ID}"
+    user_response = requests.get(url)
 
-to_do = requests.get(url_1)
-to_do_data = to_do.json()
+    # parsing the user data to json format
+    user_data = user_response.json()
 
-EMPLOYEE_NAME = user_data['name']
-no_of_tasks = str(len(to_do_data))
-completed_tasks = str(sum(1 for task in to_do_data if task.get("completed")))
+    """ a get request to get the user's
+    to do data from /users/employee_ID and parsing it to json"""
+    url_1 = f'https://jsonplaceholder.typicode.com/users/{employee_ID}/todos'
 
-print('Employee {} is done with tasks {}/{}'
-      .format(EMPLOYEE_NAME, completed_tasks, no_of_tasks))
+    to_do = requests.get(url_1)
+    to_do_data = to_do.json()
 
-# print the title of each completed task
-for task in to_do_data:
-    if task["completed"]:
-        print('\t' + task["title"])
+    EMPLOYEE_NAME = user_data['name']
+    no_of_tasks = str(len(to_do_data))
+    completed_tasks = str(sum(1 for task in to_do_data
+                              if task.get("completed")))
+
+    print('Employee {} is done with tasks {}/{}'
+          .format(EMPLOYEE_NAME, completed_tasks, no_of_tasks))
+
+    # print the title of each completed task
+    for task in to_do_data:
+        if task["completed"]:
+            print('\t' + task["title"])
+
 
 if __name__ == "__main__":
-    pass
+    todo_list(employee_ID)
